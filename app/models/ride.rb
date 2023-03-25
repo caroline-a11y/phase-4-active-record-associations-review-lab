28ae85c7b25308925e81696bff9ddc848f1c5bfc
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-03-25 22:09:12 +0300

    completed assinment

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,4 @@
 class Ride < ApplicationRecord
+    belongs_to :Passenger
+    belongs_to :taxi
 end
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,4 @@
 class Taxi < ApplicationRecord
+    has_many :rides
+    has_many :Passengers, through: :rides
 end
